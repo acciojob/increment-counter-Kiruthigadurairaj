d4f2c98c50f1e8310d1c868dca34285f8a9cf87b
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-19 19:24:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+//your JS code here. If r
+const counterParagraph = document.getElementById('counter');
+const incrementButton = document.getElementById('incrementBtn');
+let counterValue = 0;
+incrementButton.addEventListener('click', () => {
+  // Display an alert 
+  alert(`Current value: ${counterValue}`);
+counterValue++;
+  // Update the paragraph
+  counterParagraph.textContent = counterValue;
+});
